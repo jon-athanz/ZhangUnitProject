d812dfc9930cf6cd3fbac27caa5a4020ecc8038a
Jonathan Zhang <jonathanz28@nycstudents.net>
2022-12-08 00:03:25 -0500

    ZhangUnitProject

--- src/Computer.java
+++ src/Computer.java
@@ -1,37 +1,81 @@
 public class Computer {
     private int max;
     private int min;
-    private boolean won;
+    private int randomNum;
+
+    private String firstName;
+    private int first;
+    private String secondName;
+    private int second;
+    private String thirdName;
+    private int third;
 
     public Computer(int maximum, int minimum) {
         max = maximum;
         min = minimum;
-        won = false;
+        randomNum = 0;
+        first = 50;
+        second = 100;
+        third = 150;
 
     }
-
     public Computer() {
         min = 1;
         max = 100;
-        won = false;
+        randomNum = 0;
+        first = 50;
+        second = 100;
+        third = 150;
+
     }
 
-    public int randomNum() {
-        int randonNum = (int) (Math.random() * (max - min)) + min;
-        return randonNum;
+    public int getRandomNum() {
+        return randomNum;
     }
+    public void randomNum() {
+        randomNum = (int) ((Math.random() * (max - min))) + min;
+    }
+
 
     public void numberChecker(int guess) {
-        if (guess > randomNum()) {
+        if (guess > getRandomNum()) {
             System.out.println("Lower");
-        } else if (guess < randomNum()) {
+        } else if (guess < getRandomNum()) {
             System.out.println("Higher");
         } else {
             System.out.println("You got it!");
-            won = true;
         }
     }
 
+    public boolean winnerChecker(int guess) {
+        if (guess == getRandomNum()) {
+            return false;
+        } else {
+            return true;
+        }
+    }
 
+    public void checkLeaderBoardScore(int count, String name) {
+        if (count < first) {
+            second = first;
+            third = second;
+            first = count;
+            firstName = name;
+        } else if (count < second) {
+            third = second;
+            second = count;
+            secondName = name;
+        } else if (count < third) {
+            third = count;
+            thirdName = name;
+        }
+    }
 
+    public String leaderBoard() {
+        String leaders = "First Place = " + firstName + " | Score = " + first;
+        leaders += "\nSecond Place = " + secondName + " | Score = " + second;
+        leaders += "\nThird Place = " + thirdName + " | Score = " + third;
+
+        return leaders;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,46 @@ public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("This is a Number Guessing Game!");
-        System.out.println("Enter the minimum that you want: ");
-        int minimum = scan.nextInt();
-        System.out.println("Enter the maximum that you want: ");
-        int maximum = scan.nextInt();
+        int count = 0;
+        boolean notWinner = true;
 
-        Computer comp1 = new Computer(maximum, minimum);
-        comp1.randomNum();
-        System.out.println("Enter your guess: (and we will tell you Higher or Lower) ");
-        int guess = scan.nextInt();
+        System.out.println("This is a Number Guessing Game!"); // Unit 1, Requirement i
+        System.out.println("What is your name? ");
+        String name = scan.nextLine();
+        System.out.println("Do you want to enter your own range or use the default range? Type (y/n)");
+        String answer = scan.nextLine();
+        if (answer.equals("y")) {
+            System.out.println("Enter the minimum that you want: ");
+            int minimum = scan.nextInt();
+            System.out.println("Enter the maximum that you want: ");
+            int maximum = scan.nextInt();
+
+
+            Computer comp1 = new Computer(maximum, minimum);
+            comp1.randomNum();
+            while (notWinner) {
+                System.out.println("Enter your guess: (and we will tell you Higher or Lower) ");
+                int guess = scan.nextInt();
+                comp1.numberChecker(guess);
+                notWinner = comp1.winnerChecker(guess);
+                count++;
+            }
+            System.out.println(name + "'s score = " + count);
+            comp1.checkLeaderBoardScore(count, name);
+            System.out.println(comp1.leaderBoard());
+        } else {
+
+            Computer comp1 = new Computer();
+            comp1.randomNum();
+            while (notWinner) {
+                System.out.println("Enter your guess: (and we will tell you Higher or Lower) ");
+                int guess = scan.nextInt();
+                comp1.numberChecker(guess);
+                notWinner = comp1.winnerChecker(guess);
+                count++;
+            }
+            System.out.println(name + "'s score = " + count);
+        }
+
     }
 }
