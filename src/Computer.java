696a43259c2638b73dad9af07c69c682fc652121
Jonathan Zhang <jonathanz28@nycstudents.net>
2022-12-07 21:37:13 -0500

    ZhangUnitProject

--- src/Computer.java
+++ src/Computer.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class Computer {
+public class Computer {
+    private int max;
+    private int min;
+    private boolean won;
+
+    public Computer(int maximum, int minimum) {
+        max = maximum;
+        min = minimum;
+        won = false;
+
+    }
+
+    public Computer() {
+        min = 1;
+        max = 100;
+        won = false;
+    }
+
+    public int randomNum() {
+        int randonNum = (int) (Math.random() * (max - min)) + min;
+        return randonNum;
+    }
+
+    public void numberChecker(int guess) {
+        if (guess > randomNum()) {
+            System.out.println("Lower");
+        } else if (guess < randomNum()) {
+            System.out.println("Higher");
+        } else {
+            System.out.println("You got it!");
+            won = true;
+        }
+    }
+
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("This is a Number Guessing Game!");
+        System.out.println("Enter the minimum that you want: ");
+        int minimum = scan.nextInt();
+        System.out.println("Enter the maximum that you want: ");
+        int maximum = scan.nextInt();
+
+        Computer comp1 = new Computer(maximum, minimum);
+        comp1.randomNum();
+        System.out.println("Enter your guess: (and we will tell you Higher or Lower) ");
+        int guess = scan.nextInt();
     }
 }
